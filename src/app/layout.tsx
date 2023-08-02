a825bcb4b34d8532987e92518abdc8beec271e39
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-02 14:46:19 -0300

    chore(.editorconfig): adiciona *.config.js à lista de arquivos para aplicar a regra single_quote chore(.eslintrc.json): adiciona configurações do ESLint para ambiente de navegador e node, e regras adicionais chore(next.config.js): adiciona configuração para permitir imagens remotas do GitHub chore(src/app/layout.tsx): adiciona ponto e vírgula faltante e formatação do código chore(src/app/page.tsx): adiciona importação do componente Navbar e atualiza o layout da página inicial feat(src/components/navbar.tsx): adiciona componente Navbar com funcionalidades básicas chore(tailwind.config.js): atualiza tema do DaisyUI para "dark"
    As alterações foram feitas para melhorar a consistência do código, adicionar funcionalidades e melhorar a experiência do usuário.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'My Storage',
-  description: 'My Storage 1.0',
-}
+    title: 'My Storage',
+    description: 'My Storage 1.0',
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="pt-br">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from '@/components/navbar';
+
 export default function Home() {
-  return (
-    <div className='flex h-screen justify-center align-middle items-center'>
-      <p>My-Storage 1.0</p>
-    </div>
-  )
+    return (
+        <main className='flex flex-col h-screen'>
+            <Navbar />
+            <div className='flex-grow bg-slate-700 flex justify-center items-center'>
+                <p className='text-white text-xl font-bold'>My-Storage 1.0</p>
+            </div>
+        </main>
+    );
 }
