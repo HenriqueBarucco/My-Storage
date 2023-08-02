619e2c8f6cf201f1a3a53bdac44525e7262ddcec
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-02 14:53:43 -0300

    feat(navbar.tsx): adiciona botão de menu dropdown Adiciona um botão de menu dropdown à barra de navegação. O botão é representado por um ícone de três linhas horizontais e permite que o usuário acesse opções adicionais ou menus suspensos.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,6 +11,13 @@ export default function Navbar() {
                     <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
                 </div>
                 <div className="dropdown dropdown-end">
+                    <button className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                    </button>
+                </div>
+                <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <Image src={'https://github.com/henriquebarucco.png'} alt='HB' width={720} height={720}/>                       
