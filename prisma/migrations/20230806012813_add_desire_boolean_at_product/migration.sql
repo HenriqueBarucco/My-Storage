dfdc5ec50949cb09c75e97bc94c7bd038c4e86e2
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-05 22:38:56 -0300

    feat: adiciona coluna 'desire' à tabela 'products_tbl' com valor padrão false feat: adiciona campo 'desire' ao modelo 'Product' no schema.prisma fix: filtra produtos na lista de produtos com base no campo 'desire' feat: adiciona funcionalidade de alternar entre 'Meus Produtos' e 'Lista de Desejos' na lista de produtos

---
+++ prisma/migrations/20230806012813_add_desire_boolean_at_product/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `products_tbl` ADD COLUMN `desire` BOOLEAN NOT NULL DEFAULT false;
--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -6,7 +6,8 @@ import ProductCard from './product-card';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function ProductsList({ products }: { products: Product[]}) {
-    const [productList, setProducts] = useState(products);
+    const [productList, setProducts] = useState(products.filter((product) => !product.desire));
+    const [desire, setDesire] = useState(false);
     const searchParams = useSearchParams();
     const router = useRouter();
     const pathname = usePathname();
@@ -22,11 +23,24 @@ export default function ProductsList({ products }: { products: Product[]}) {
         }
     }, [search, pathname, router, products]);
 
+    useEffect(() => {
+        if (desire) {
+            setProducts(products.filter((product) => product.desire));
+        } else {
+            setProducts(products.filter((product) => !product.desire));
+        }
+    }, [desire, products]);
+
     return (
-        <div className="flex flex-wrap max-w-full p-14 lg:justify-center">
-            {productList.map((product, index) => (
-                <ProductCard key={index} product={product as any}/>
-            ))}
-        </div>
+        <>
+            <div className="tabs justify-center pt-8">
+                <a className={`tab tab-lifted ${desire ? '' : 'tab-active'}`} onClick={() => desire && setDesire(!desire)}>Meus Produtos</a> 
+                <a className={`tab tab-lifted ${desire ? 'tab-active' : ''}`} onClick={() => !desire && setDesire(!desire)}>Lista de Desejos</a> 
+            </div>
+            <div className="flex flex-wrap max-w-full p-14 lg:justify-center">
+                {productList.map((product, index) => (
+                    <ProductCard key={index} product={product as any}/>
+                ))}
+            </div></>
     );
 }
