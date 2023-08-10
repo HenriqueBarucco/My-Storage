dceb9aadef0a0d12ad39626efe45dba7732b9f26
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-10 17:17:39 -0300

    refactor(navbar.tsx): remove a importação não utilizada de Image do next/image refactor(navbar.tsx): substitui a imagem do avatar por um ícone SVG A importação não utilizada de Image do next/image foi removida para melhorar a organização do código. A imagem do avatar foi substituída por um ícone SVG para reduzir a dependência de recursos externos e melhorar o desempenho do aplicativo.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
-import Image from 'next/image';
 import AddProductModal from './add-product/add-product-modal';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
@@ -53,13 +52,10 @@ export default function Navbar({ name }: { name: string }) {
                         tabIndex={0}
                         className="btn btn-ghost btn-circle avatar"
                     >
-                        <div className="w-10 rounded-full">
-                            <Image
-                                src={'https://images.henriquebarucco.com.br/X22Lv.png'}
-                                alt={name}
-                                width={720}
-                                height={720}
-                            />
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-8 h-8 text-center stroke-primary justify-center">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
                         </div>
                     </label>
                     <ul
